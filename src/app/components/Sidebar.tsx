88000e8f78d442703e6df2112b68b9fb7e59b03b
Bruno Borges <bruno.s.borges96@gmail.com>
2023-05-07 00:09:52 -0300

    first commit

---
+++ src/app/components/Sidebar.tsx
@@ -0,0 +1,42 @@
+import { HomeIcon, Library, Search } from 'lucide-React';
+
+export function Sidebar(){
+    return(<aside className="w-72 bg-zinc-950 p-6">
+
+          <div className='flex items-center gap-2'>
+            <div className='w-3 h-3 bg-red-500 rounded-full'/>
+            <div className='w-3 h-3 bg-yellow-500 rounded-full'/>
+            <div className='w-3 h-3 bg-green-500 rounded-full'/>
+          </div>
+
+          <nav className='space-y-5 mt-10'>
+            <a href="" className='flex items-center gap-3 text-sm font-semibold text-zinc-200'>
+              <HomeIcon/>
+              Home
+            </a>
+            <a href="" className='flex items-center gap-3 text-sm font-semibold text-zinc-200'>
+              <Search/>
+              Search
+            </a>
+            <a href="" className='flex items-center gap-3 text-sm font-semibold text-zinc-200'>
+              <Library/>
+              Your Library
+            </a>
+          </nav>
+
+          <nav className='mt-6 pt-6 border-t border-zinc-800 flex flex-col gap-3'>
+            <a href="" className='text-sm text-zinc-400 hover:text-zinc-100'>GR6 EXPLODE 2023 - PLAYLIST</a>
+            <a href="" className='text-sm text-zinc-400 hover:text-zinc-100'>Project X - Movie Soundtrack</a>
+            <a href="" className='text-sm text-zinc-400 hover:text-zinc-100'>Falatu Playlist</a>
+            <a href="" className='text-sm text-zinc-400 hover:text-zinc-100'>Inter</a>
+            <a href="" className='text-sm text-zinc-400 hover:text-zinc-100'>RITMO DOS BAILES 2022</a>
+            <a href="" className='text-sm text-zinc-400 hover:text-zinc-100'>Se ouvir vai chorar</a>
+            <a href="" className='text-sm text-zinc-400 hover:text-zinc-100'>RAP NACIONAL</a>
+            <a href="" className='text-sm text-zinc-400 hover:text-zinc-100'>Mix Relax</a>
+            <a href="" className='text-sm text-zinc-400 hover:text-zinc-100'>Flow Romance</a>
+            <a href="" className='text-sm text-zinc-400 hover:text-zinc-100'>AMPLIFIKA</a>
+          </nav>
+
+
+        </aside>)
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,118 @@
+import{Home as ChevronLeft, ChevronRight, Play} from 'lucide-React'
+import Image from 'next/image'
+import { Sidebar } from './components/Sidebar'
+import { Footer } from './components/Footer'
+
+export default function Home() {
+  return (
+    <div className="h-screen flex flex-col">
+      <div className="flex flex-1">
+        <Sidebar/>
+        
+        <main className="flex-1 p-6">
+          <div className='flex items-center gap-3'>
+            <button className='rounded-full bg-black/40 p-1'>
+              <ChevronLeft/>
+            </button>
+            <button className='rounded-full bg-black/40 p-1'>
+              <ChevronRight/>
+            </button>
+          </div>
+
+          <h1 className='font-semibold text-3xl mt-10'>Good Afternoon</h1>
+
+          <div className='grid grid-cols-3 gap-4 mt-4'>
+            <a href='#' className='bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors'>
+              <Image src="/Album2.jpg" width={104} height={104} alt="Capa do Album do Post Malone"/>
+              <strong>Post Malone</strong>
+              <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible '>
+                <Play fill='bg-black'/>
+              </button>
+            </a>
+            <a href='#' className='bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors'>
+              <Image src="/ImagineDragons.jpg" width={104} height={104} alt="Capa do Album do Post Malone"/>
+              <strong>Imagine Dragons</strong>
+              <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible '>
+                <Play fill='bg-black'/>
+              </button>
+            </a>
+            <a href='#' className='bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors'>
+              <Image src="/Justin.jpg" width={104} height={104} alt="Capa do Album do Post Malone"/>
+              <strong>Justin Bieber</strong>
+              <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible '>
+                <Play fill='bg-black'/>
+              </button>
+            </a>
+            <a href='#' className='bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors'>
+              <Image src="/RBD.jpg" width={104} height={104} alt="Capa do Album do Post Malone"/>
+              <strong>RBD</strong>
+              <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible '>
+                <Play fill='bg-black'/>
+              </button>
+            </a>
+            <a href='#' className='bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors'>
+              <Image src="/Rosalia.jpg" width={104} height={104} alt="Capa do Album do Post Malone"/>
+              <strong>Rosalía</strong>
+              <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible '>
+                <Play fill='bg-black'/>
+              </button>
+            </a>
+            <a href='#' className='bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors'>
+              <Image src="/Travis.jpg" width={104} height={104} alt="Capa do Album do Post Malone"/>
+              <strong>Travis Scott</strong>
+              <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible '>
+                <Play fill='bg-black'/>
+              </button>
+            </a>
+          </div>
+
+          <h2 className='font-semibold text-2xl mt-10'>Made for Bruno Borges</h2>
+
+          <div className='grid grid-cols-8 gap-4 mt-4'>
+            <a href='#' className='bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10'>
+              <Image src="/Daily1.jpg" className='w-full' width={120} height={120} alt="Capa do Album do Post Malone"/>
+              <strong className='font-semibold'>Daily Mix 1</strong>
+              <span className='text-sm text-zinc-500'>Kayblack, Tz da Coronel, Israel &...</span>
+            </a>
+            <a href='#' className='bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10'>
+              <Image src="/Daily2.jpg" className='w-full' width={120} height={120} alt="Capa do Album do Post Malone"/>
+              <strong className='font-semibold'>Daily Mix 2</strong>
+              <span className='text-sm text-zinc-500'>Costa Gold, PrimeiraMente, Aka...</span>
+            </a>
+            <a href='#' className='bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10'>
+              <Image src="/Daily3.jpg" className='w-full' width={120} height={120} alt="Capa do Album do Post Malone"/>
+              <strong className='font-semibold'>Daily Mix 3</strong>
+              <span className='text-sm text-zinc-500'>Imagine Dragons, Yeah Yeah Yeahs,...</span>
+            </a>
+            <a href='#' className='bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10'>
+              <Image src="/Daily4.jpg" className='w-full' width={120} height={120} alt="Capa do Album do Post Malone"/>
+              <strong className='font-semibold'>Daily Mix 4</strong>
+              <span className='text-sm text-zinc-500'>Fugees, 50 Cent, Busta Rhymes e mais</span>
+            </a>
+            <a href='#' className='bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10'>
+              <Image src="/Daily5.jpg" className='w-full' width={120} height={120} alt="Capa do Album do Post Malone"/>
+              <strong className='font-semibold'>Daily Mix 5</strong>
+              <span className='text-sm text-zinc-500'>Filipe Ret, Selminho DJ, MC MENOR H...</span>
+            </a>
+            <a href='#' className='bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10'>
+              <Image src="/Daily6.jpg" className='w-full' width={120} height={120} alt="Capa do Album do Post Malone"/>
+              <strong className='font-semibold'>Descobertas da Semana</strong>
+              <span className='text-sm text-zinc-500'>Seu mixtape semanal cheia de novidades</span>
+            </a>
+            <a href='#' className='bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10'>
+              <Image src="/Daily8.jpg" className='w-full' width={120} height={120} alt="Capa do Album do Post Malone"/>
+              <strong className='font-semibold'>Radar de Novidades</strong>
+              <span className='text-sm text-zinc-500'>Confira os lançamentos dos artistas que você...</span>
+            </a>
+            <a href='#' className='bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10'>
+              <Image src="/Mix.jpg" className='w-full' width={120} height={120} alt="Capa do Album do Post Malone"/>
+              <strong className='font-semibold'>Mix de Mc IG</strong>
+              <span className='text-sm text-zinc-500'>Veigh, WIU e MD Chefe</span>
+            </a>
+          </div>
+        </main>
+      </div>
+      <Footer/>
+    </div>
+  )
+}
